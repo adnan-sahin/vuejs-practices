b10759141ecdeb80a3a18a0f5b891c7f8e2b3fc1
Adnan Sahin <adnansahin1@outlook.com>
2019-01-20 11:23:14 +0300

    injecting content into a  component with slots

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="content">
     <div class="preview">
-      <div class="preview-content">
-        <div class="top-row">
-          <img :src="selectedRobot.head.src">
+      <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src">
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left">
+            <img :src="selectedRobot.torso.src">
+            <img :src="selectedRobot.rightArm.src" class="rotate-right">
+          </div>
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src">
+          </div>
         </div>
-        <div class="middle-row">
-          <img :src="selectedRobot.leftArm.src" class="rotate-left">
-          <img :src="selectedRobot.torso.src">
-          <img :src="selectedRobot.rightArm.src" class="rotate-right">
-        </div>
-        <div class="bottom-row">
-          <img :src="selectedRobot.base.src">
-        </div>
-      </div>
+      </CollapsibleSection>
+
       <button class="add-to-cart" @click="addToCart()">Add To Cart</button>
     </div>
 
@@ -79,13 +82,14 @@
 import availableParts from '../data/parts';
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
+import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
   // created() {
   //   console.log('component created');
   // },
-  components: { PartSelector },
+  components: { PartSelector, CollapsibleSection },
   mixins: [createdHookMixin],
   data() {
     return {
---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="open=!open">&#x25B2; Collapse</span>
+      <span v-if="!open" @click="open=!open">&#x25BC; Expand</span>
+    </div>
+    <!--Toggleable-->
+    <slot v-if="open">Default Content</slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #bbb;
+  padding: 3px;
+  cursor: pointer;
+}
+</style>
