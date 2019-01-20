4274db0430faf5bdb5ac5446603f69055386b474
Adnan Sahin <adnansahin1@outlook.com>
2019-01-20 19:59:12 +0300

    passing params as props

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -10,9 +10,19 @@ import parts from '../data/parts';
 
 export default {
   name: 'PartInfo',
+  props: {
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+    partType: { type: String },
+  },
   computed: {
     part() {
-      const { id, partType } = this.$route.params;
+      //   const { id, partType } = this.$route.params;
+      const { id, partType } = this;
       return parts[partType].find(part => part.id === +id);
     },
   },
--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ export default new Router({
       path: '/parts/:partType/:id',
       name: 'Parts',
       component: PartInfo,
+      props: true,
     },
   ],
 });
