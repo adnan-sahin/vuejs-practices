77a91c940615ef8d2980de29af81210cd3cfe413
Adnan Sahin <adnansahin1@outlook.com>
2019-01-20 09:46:33 +0300

    child and parent components communication

---
+++ src/build/RobotBuilder.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCart()">Add To Cart</button>
+    <div class="top-row">
+      <!-- <div class="top part" :style="headBorderStyle"> -->
+      <!-- <div :class="[saleBorderClass,'top', 'part']">
+        <div class="robot-name">
+          {{robotName}}
+          <span v-show="selectedRobot.head.onSale" class="sale">Sale!</span>
+        </div>
+      </div>-->
+      <PartSelector :parts="availableParts.heads" position="top"/>
+    </div>
+    <div class="middle-row">
+      <PartSelector :parts="availableParts.arms" position="left"/>
+      <PartSelector :parts="availableParts.torsos" position="center"/>
+      <PartSelector :parts="availableParts.arms" position="right"/>
+    </div>
+    <div class="bottom-row">
+      <PartSelector :parts="availableParts.bases" position="bottom"/>
+    </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot,index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
+import PartSelector from './PartSelector.vue';
+
+export default {
+  name: 'RobotBuilder',
+  // created() {
+  //   console.log('component created');
+  // },
+  components: { PartSelector },
+  mixins: [createdHookMixin],
+  data() {
+    return {
+      availableParts,
+      cart: [],
+      selectedRobot: {
+        head: {},
+        leftArm: {},
+        torso: {},
+        rightArm: {},
+        base: {},
+      },
+    };
+  },
+  computed: {
+    robotName() {
+      return this.selectedRobot.head.title;
+    },
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale
+          ? '3px solid red'
+          : '3px solid #aaa',
+      };
+    },
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+  },
+  methods: {
+    addToCart() {
+      const robot = this.selectedRobot;
+      const cost =        robot.head.cost
+        + robot.leftArm.cost
+        + robot.rightArm.cost
+        + robot.torso.cost
+        + robot.base.cost;
+      this.cart.push(Object.assign({}, robot, { cost }));
+      console.log(this.cart);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.part {
+  position: relative;
+  width: 165px;
+  height: 165px;
+  border: 3px solid #aaa;
+}
+.part {
+  img {
+    width: 165px;
+  }
+}
+.top-row {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 25px;
+}
+.middle-row {
+  display: flex;
+  justify-content: center;
+}
+.bottom-row {
+  display: flex;
+  justify-content: space-around;
+  border-top: none;
+}
+.head {
+  border-bottom: none;
+}
+.left {
+  border-right: none;
+}
+.right {
+  border-left: none;
+}
+.left img {
+  transform: rotate(-90deg);
+}
+.right img {
+  transform: rotate(90deg);
+}
+.bottom {
+  border-top: none;
+}
+.prev-selector {
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 171px;
+}
+.next-selector {
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 171px;
+}
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
+}
+.left .prev-selector {
+  top: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.left .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.right .prev-selector {
+  top: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  right: -3px;
+}
+
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
+}
+.sale {
+  color: red;
+}
+.content {
+  position: relative;
+}
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 7px;
+  font-size: 16px;
+}
+td,
+th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+.cost {
+  text-align: right;
+}
+.sale-border {
+  border: 3px solid red;
+}
+</style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" alt aria-hidden="true">
+    </div>
+    <div>
+      <a href>Get Started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+</style>
